361a87a5c237833dc24c56946e7c8713f663d428
Born2shine <odohfriday33@yahoo.com>
2019-11-21 03:17:36 +0800

    another changes...

--- app/Http/Controllers/ATGController.php
+++ app/Http/Controllers/ATGController.php
@@ -19,7 +19,7 @@ class ATGController extends Controller
         // }
         $this->validate(request(), [
             'name' => 'required',
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email:dns|unique:users',
             'pincode' => 'required|unique:users|size:6'
         ]);
 
